dfaecfc7ee6ccb3db8743473c754deed28c34bf2
Andres2310 <and.23108@gmail.com>
2022-09-03 02:02:14 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="App bg-stone-800 min-h-screen flex justify-center p-4 font-semibold text-4xl">
+      <h1 className='text-emerald-500'>Alessandri Music</h1>
+    </div>
+  )
+}
+
+export default App
