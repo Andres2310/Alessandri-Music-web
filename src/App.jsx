c48a60c89148b6ec3db5d9ca43e2cc045aaed42c
Andres2310 <and.23108@gmail.com>
2022-09-03 13:00:09 -0400

    modify layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
 
   return (
-    <div className="App bg-stone-800 min-h-screen flex justify-center p-4 font-semibold text-4xl">
-      <h1 className='text-emerald-500'>Alessandri Music</h1>
+    <div className="App bg-stone-800 min-h-screen flex flex-col items-center p-4 ">
+      <h1 className='text-emerald-500 font-semibold text-4xl'>Alessandri Music</h1>
+      <p className="text-slate-200 font-medium text-2xl">Escuchelo en Spotify</p>
     </div>
   )
 }
