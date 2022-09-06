83cadb52eedeb6a6843d37d5e9d5ac97c2f522aa
Andres2310 <and.23108@gmail.com>
2022-09-06 01:00:02 -0400

    Basic Header Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./Header"
+
 function App() {
 
   return (
-    <div className="App bg-stone-800 min-h-screen flex flex-col items-center p-4 ">
-      <h1 className='text-emerald-500 font-semibold text-4xl'>Alessandri Music</h1>
-      <p className="text-slate-200 font-medium text-2xl">Escuchelo en Spotify</p>
+    <div className="App bg-neutral-900 min-h-screen flex flex-col items-center ">
+      <Header/>
     </div>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,40 @@
+import {CgFacebook} from "react-icons/cg"
+import {RiSpotifyFill} from "react-icons/ri"
+import {TiSocialInstagram,TiSocialYoutube} from "react-icons/ti"
+function Header() {
+  
+  return (
+    
+    <header className="min-w-full min-h-[200px] bg-white flex flex-col items-center">
+      <div className="max-w-sm min-h-full p-6 pb-0 mb-0">
+        <img src="../resources/LogoAlessandri-Negro-06.png" alt="Alessandri-logo" />
+      </div>
+      {/* <span>Menu</span> */}
+      <nav className="flex justify-center gap-5 pt-2 text-base font-semibold">
+        <a href="">MÚSICA</a>
+        <a href="">MERCH</a>
+      </nav>
+      <footer className="container text-center p-5">
+        <div className="flex justify-center gap-3">
+          <a href="https://www.facebook.com/alessandri.aam" target="_blank" type="button" className="rounded-full bg-stone-900 text-white leading-normal uppercase hover:bg-stone-700 hover:bg-opacity-2 focus:outline-none transition duration-150 ease-in-out w-7 h-7 m-1">
+            <CgFacebook className="w-5 h-full mx-auto"/>
+          </a>
+
+          <a href="https://www.instagram.com/alessandri.aam/" target="_blank" type="button" className="rounded-full bg-stone-900 text-white leading-normal uppercase hover:bg-stone-700 hover:bg-opacity-2 focus:outline-none transition duration-150 ease-in-out w-7 h-7 m-1">
+          <TiSocialInstagram className="w-5 h-full mx-auto"/>
+          </a>
+
+          <a href="https://www.youtube.com/channel/UCGQVACszhJiMmSXPZ9NA1Sw" target="_blank" type="button" className="rounded-full bg-stone-900 text-white leading-normal uppercase hover:bg-stone-700 hover:bg-opacity-2 focus:outline-none transition duration-150 ease-in-out w-7 h-7 m-1">
+          <TiSocialYoutube className="w-5 h-full mx-auto"/>
+          </a>
+          
+          <a href="https://open.spotify.com/artist/2sLAM8ozPbTzx661cN2QyQ" target="_blank" type="button" className="rounded-full bg-stone-900 text-white leading-normal uppercase hover:bg-stone-700 hover:bg-opacity-2 focus:outline-none transition duration-150 ease-in-out w-7 h-7 m-1">
+          <RiSpotifyFill className="w-5 h-full mx-auto"/>
+          </a>
+        </div>
+      </footer>
+    </header>
+  )
+}
+
+export default Header
