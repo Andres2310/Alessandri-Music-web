1132080c59d14bd8da90519fda3bed609d36eb28
Andres2310 <and.23108@gmail.com>
2022-09-26 21:58:14 -0400

    enhancement: change images url

--- src/components/CarouselAlbums.jsx
+++ src/components/CarouselAlbums.jsx
@@ -6,7 +6,7 @@ function CarouselAlbums() {
   return (
     <Carousel className="max-w-4xl sm: w-full" autoPlay interval={2000} infiniteLoop={true} autoFocus={true} showStatus={false} showThumbs={false}>
       <div>
-        <img src="https://res.cloudinary.com/anubito/image/upload/v1662483269/Imagenes/Portadas/Rompe-el-silencio_han5fw.png" alt="album" />
+        <img src="https://res.cloudinary.com/anubito/image/upload/v1664239225/Imagenes/fotos%20otras/otras%20fotos/Rompe_el_silencio-min_k72rsj.jpg" alt="album" />
         <div className="flex p-1 gap-3 -translate-y-16 translate-x-4">
 
           <button className="text-white m-1 bg-rose-700 hover:bg-rose-500 focus:outline-none font-medium rounded-full text-lg font-serif py-1 px-2.5 sm:text-2xl">Compralo</button>
@@ -16,7 +16,7 @@ function CarouselAlbums() {
       </div>
 
       <div>
-        <img src="https://res.cloudinary.com/anubito/image/upload/v1662483226/Imagenes/Portadas/M%C3%A1s-all%C3%A1-de-la-raz%C3%B3n_uzubae.png" alt="album" />
+        <img src="https://res.cloudinary.com/anubito/image/upload/v1664239225/Imagenes/fotos%20otras/otras%20fotos/M%C3%A1s_all%C3%A1_de_la_raz%C3%B3n-min_esyznd.jpg" alt="album" />
         <div className="flex p-1 gap-3 -translate-y-16 translate-x-3">
 
           <button className="text-white m-1 bg-rose-700 hover:bg-rose-500 focus:outline-none font-medium rounded-full text-lg font-serif py-1 px-2.5 sm:text-2xl">Compralo</button>
@@ -26,7 +26,7 @@ function CarouselAlbums() {
       </div>
 
       <div>
-        <img src="https://res.cloudinary.com/anubito/image/upload/v1662483225/Imagenes/Portadas/Vive-Final_unhupe.png" alt="album" />
+        <img src="https://res.cloudinary.com/anubito/image/upload/v1664239226/Imagenes/fotos%20otras/otras%20fotos/Vive_Final-min_esu23f.jpg" alt="album" />
         <div className="flex p-1 gap-3 -translate-y-16 translate-x-3">
 
           <button className="text-white m-1 bg-rose-700 hover:bg-rose-500 focus:outline-none font-medium rounded-full text-lg font-serif py-1 px-2.5 sm:text-2xl">Compralo</button>
