a5be3a5287a95557468d1f4b114c8f9afa42c755
unknown <and.23108@gmail.com>
2024-09-02 01:28:53 -0400

    change: new aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@
 function AboutMe() {
   return (
     <div className="container flex flex-col">
-      <h2 className="text-white font-semibold font-mono text-3xl p-1">Sobre mi</h2>
+      <h2 className="text-white font-semibold font-mono text-3xl p-1">Sobre mi Alessandri</h2>
 
     </div>
   )
